919d60eca4ef38912b283f224361c87096d18a1e
Denilson Barboza da Silva <89758699+denilsonbslv@users.noreply.github.com>
2024-03-26 06:39:03 -0300

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillMember() {
+    var member = {
+        name: 'John',
+        age: 30,
+        skills: ['JavaScript', 'React', 'Node'],
+        details: function() {
+            console.log(this.name + ' is ' + this.age + ' years old. He knows ' + this.skills);
+        }
+    }
+    return member;
+}
